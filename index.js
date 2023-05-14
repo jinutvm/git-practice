f76bcc34612d698ac5d924ba6d7febc3a5bb91ff
jinu babu <jinutvm@gmail.com>
2023-05-14 07:30:45 +0530

    Version 4.0

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-console.log('Version3.0.2')
-=======
-console.log('Version3.2')
-console.log('feature1')
->>>>>>> feature1
+console.log('Version4.0')
+console.log('feature2')
--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-console.log('Version3.2')
-console.log('feature1')
+console.log('Version4.0')
+console.log('feature2')
