bd76f0b358bbb6409ad175408edc2a3bb0050106
jinu babu <jinutvm@gmail.com>
2023-05-14 12:18:04 +0530

    Version 5.0

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
-console.log('Version4.6.1')
+console.log('Version5.0')
 console.log('feature2')
 console.log('feature4.3')
 console.log('feature4.6')
 console.log('feature4.6.1')
+console.log('feature5')
--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-console.log('Version4.6.1')
+console.log('Version5.0')
 console.log('feature2')
 console.log('feature4.3')
 console.log('feature4.6')
